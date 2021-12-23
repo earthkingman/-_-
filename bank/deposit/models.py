bf932c6d40e6579637b99f1f930b0997c996e3f4
ji-park <qkrskdjf@naver.com>
2021-12-23 20:43:22 +0900

    Feat : 출금 입금 계좌 유저 APP 생성

--- bank/deposit/models.py
+++ bank/deposit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Deposit(models,Model):
+    id = models.IntegerField
+    new_balance = models.PositiveBigIntegerField
+    old_balance = models.PositiveBigIntegerField
+    amount = models.PositiveBigIntegerField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
