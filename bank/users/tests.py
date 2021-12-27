be1173872692f2f89ca3cfa26094b343d94cdede
ji-park <qkrskdjf@naver.com>
2021-12-28 07:16:15 +0900

    Feat : 로그인 회원가입 테스팅 #3

--- bank/users/tests.py
+++ bank/users/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+import bcrypt
+from unittest.mock import MagicMock, patch
+import json
+from .models import User
 # Create your tests here.
+
+class SignUpTest(TestCase):
+    def test_signup_success(self):
+        client = Client()
+        user = {
+            "email": "Mark1@stark.com",
+            "password": "mark486**",
+        }
+        response = client.post(
+            "/users/signup", json.dumps(user), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {"Message": "SUCCESS"})
+
+    def setUp(self):
+        User.objects.create(
+            email="ji-park@42seoul.com",
+            password="42seoul",
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+      
+    def test_signupview_post_duplicated_user(self):
+        client = Client()
+        user = {
+            "email": "ji-park@42seoul.com",
+            "password": "42seoul",
+        }
+        response = client.post(
+            "/users/signup", json.dumps(user), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"Message": "USER_ALREADY_EXISTS"})
+
+class LoginTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            email="ji-park@42seoul.com",
+            password=bcrypt.hashpw('42seoul'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+        )
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_login_success(self):
+        client = Client()
+        user = {
+            "email" : "ji-park@42seoul.com",
+            "password" : "42seoul",
+        }
+
+        response = client.post(
+            "/users/login", json.dumps(user), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_unregistered_user(self):
+        client = Client()
+        user = {
+            "email" : "ji-park2@42seoul.com",
+            "password" : "42seoul",
+        }
+        response = client.post(
+            "/users/login", json.dumps(user), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"Message": "USER_DOES_NOT_EXIST"})
+
+
+    def test_login_invalid_password(self):
+        client = Client()
+        user = {
+            "email" : "ji-park@42seoul.com",
+            "password" : "42seoul2",
+        }
+        response = client.post(
+            "/users/login", json.dumps(user), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"Message": "INVALID_PASSWORD"})
--- bank/users/views.py
+++ bank/users/views.py
@@ -1,4 +1,5 @@
 import json, re, bcrypt, jwt
+from datetime import datetime, timedelta
 from django.http  import JsonResponse
 from users.models import User
 from django.shortcuts import render
@@ -7,34 +8,41 @@ SECRET_KEY = my_settings.SECRET
 # Create your views here.
 
 def signup(request):
-    try:
-        print(request.body)
-        data = json.loads(request.body)
-        print(data)
-        hash_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
-        if User.objects.filter(email=data['email']).exists(): 
-            return JsonResponse({'message':'INVALID_EMAIL'},status=400)
-        User.objects.create(
-                email        = data['email'],
-                password     = hash_password
-        )
-        return JsonResponse({'message':'SUCCESS'},status=201)
-    except KeyError:
-        return JsonResponse({'message':'ERROR'},status=400)
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            hash_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+            if User.objects.filter(email=data['email']).exists(): 
+                return JsonResponse({'Message':'USER_ALREADY_EXISTS'},status=400)
+            User.objects.create(
+                    email        = data['email'],
+                    password     = hash_password
+            )
+            return JsonResponse({'Message':'SUCCESS'},status=201)
+        except KeyError:
+            return JsonResponse({'Message':'ERROR'},status=400)
+    else :
+        return JsonResponse({'Message':'ERROR'},status=400)
 
 def login(request):
+    if request.method == 'POST':
         try :
             data = json.loads(request.body)
             if not User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'message':'INVALID_EMAIL'},status=401)
+                return JsonResponse({'Message':'USER_DOES_NOT_EXIST'},status=401)
 
             user = User.objects.get(email= data['email'])
 
             if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
-                access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm="HS256")
+                access_token = jwt.encode({'id': user.id
+                , "exp": datetime.utcnow() + timedelta(minutes=30)}
+                , SECRET_KEY
+                , algorithm="HS256")
 
                 return JsonResponse({'TOKEN': access_token},status=200)
 
-            return JsonResponse({'message':'INVALID_USER'},status=401)
+            return JsonResponse({'Message':'INVALID_PASSWORD'},status=401)
         except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'},status=400)
+            return JsonResponse({'Message':'KEY_ERROR'},status=400)
+    else :
+        return JsonResponse({'Message':'ERROR'},status=400)
